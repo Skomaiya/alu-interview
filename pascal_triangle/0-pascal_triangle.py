b2cf8d2f96764f6c0165887c91bcf6e13fc6284b
Skomaiya <s.komaiya@alustudent.com>
2024-02-19 08:24:28 +0000

    task 0

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This function looks to return a set a numbers that represent the Pascal Triangle
+"""
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    # Print the triangle for testing
+    for row in triangle:
+        print(row)
+
+# Example usage:
+pascal_triangle(7)
