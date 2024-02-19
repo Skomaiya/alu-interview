cc8d526579dd2a78239f5e7de3bbf9738e44d910
Skomaiya <s.komaiya@alustudent.com>
2024-02-19 09:22:47 +0000

    task 0

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-This function looks to return a set a numbers that represent the Pascal Triangle
+This returns a set of numbers that represent the Pascal Triangle
 """
+
+
 def pascal_triangle(n):
     """
     Generates Pascal's triangle up to the nth row.
@@ -16,11 +18,12 @@ def pascal_triangle(n):
         if i > 0:
             prev_row = triangle[i - 1]
             for j in range(1, i):
-                row.append(prev_row[j - 1] + prev_row[j])  # Calculate each element based on the previous row
+                row.append(prev_row[j - 1] + prev_row[j])
             row.append(1)  # Last element of each row is always 1
         triangle.append(row)
 
     return triangle
+
 
 def print_triangle(triangle):
     """
